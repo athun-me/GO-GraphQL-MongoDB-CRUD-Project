dc738989c0f5221079058614ca28603ebec1ec0d
athunlal <athunlalp@gmail.com>
2023-09-05 10:05:02 +0530

    mongo integrated

--- database/database.go
+++ database/database.go
@@ -1,1 +1,16 @@
 package database
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var connectinString string = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1"
+
+type DB struct {
+	client *mongo.Client
+}
+
+func connect() *DB {
+	client, err := mongo.NewClient(options.Client().ApplyURI(connectinString))
+}
